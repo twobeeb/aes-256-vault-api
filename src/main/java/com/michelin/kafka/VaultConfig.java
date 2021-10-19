88769ef98856bbed12777204eee9e884e31296fc
Twobeeb <chanaud.julien@gmail.com>
2021-10-20 00:59:32 +0200

    Initial commit

---
+++ src/main/java/com/michelin/kafka/VaultConfig.java
@@ -0,0 +1,72 @@
+package com.michelin.kafka;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.EachProperty;
+import io.micronaut.context.annotation.Parameter;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.validation.constraints.NotBlank;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import java.util.Objects;
+
+@EachProperty("vaults")
+public class VaultConfig {
+
+    private final String name;
+    @NotBlank
+    private String key;
+    @NotBlank
+    private String salt;
+
+    public VaultConfig(@Parameter String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String encryptAndFormat(String strToEncrypt) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(key.toCharArray(), salt.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/michelin/kafka/VaultController.java
@@ -0,0 +1,49 @@
+package com.michelin.kafka;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import jakarta.inject.Inject;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Controller("/vaults")
+public class VaultController {
+    @Inject
+    List<VaultConfig> vaultConfigs;
+
+    @Get
+    List<String> listVaults(){
+        return vaultConfigs.stream()
+                .map(VaultConfig::getName)
+                .collect(Collectors.toList());
+    }
+
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Post(value = "/{vault}")
+    String vaultPassword(String vault, @Body String password){
+        VaultConfig vaultConfig = vaultConfigs.stream()
+                .filter(v -> vault.equals(v.getName()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException(String.format("Vault '%s' not found", vault)));
+
+        return vaultConfig.encryptAndFormat(password);
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Post(value = "/{vault}")
+    String vaultPassword(String vault, @Valid @Body Password body){
+        return vaultPassword(vault, body.password);
+    }
+
+    @Introspected
+    static class Password {
+        @NotBlank
+        public String password;
+    }
+
+}
