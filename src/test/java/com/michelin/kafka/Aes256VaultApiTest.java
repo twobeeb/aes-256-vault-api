c424067a4423a36e3b60024e2aac63866fefa5b9
Julien Chanaud <twobeeb>
2021-10-19 21:34:56 +0000

    Initial commit

---
+++ src/test/java/com/michelin/kafka/Aes256VaultApiTest.java
@@ -0,0 +1,21 @@
+package com.michelin.kafka;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Aes256VaultApiTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
